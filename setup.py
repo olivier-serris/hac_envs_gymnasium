6ddbe6b294a1febfd96158ed210f723599e0257a
serris <olivier.serris@wanadoo.fr>
2023-08-22 16:42:48 +0200

    Added gymnasium-robotics dependency

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name="hac_envs_gymnasium",
-      packages=find_packages(),
-      version="1.0",
-      install_requires=["numpy", "gymnasium", "mujoco-py==2.1.2.14"],
-      include_package_data=True
+setup(
+    name="hac_envs_gymnasium",
+    packages=find_packages(),
+    version="1.0",
+    install_requires=[
+        "numpy",
+        "gymnasium",
+        "mujoco-py==2.1.2.14",
+        "gymnasium-robotics[mujoco-py]",
+    ],
+    include_package_data=True,
 )
